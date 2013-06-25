e1640ebb2acbcfab437a0d65e03be05d20c456c2
penjandra <pavankumarpenjandra@gmail.com>
2013-06-25 20:56:51 +0530

    table name changed

--- Labs/HDFS_LOAD_DATALOADER/verify_data_load.sh
+++ Labs/HDFS_LOAD_DATALOADER/verify_data_load.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Verify one of the GPXF/HDFS tables
-PAGER=cat psql -c "SELECT * FROM retail_demo.orders LIMIT 5"
+PAGER=cat psql -c "SELECT * FROM retail_demo.retail_demo.orders_hawq LIMIT 5"
 
