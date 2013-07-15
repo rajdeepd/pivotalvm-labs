5896586719ba4b3b76a0290591ded9d713aff521
Rajdeep Dua <dua_rajdeep@yahoo.com>
2013-07-15 08:23:45 +0530

    Fixed small typo in the script

--- Labs/HAWQ_DDL_QUERY/load_hawq_tables.sh
+++ Labs/HAWQ_DDL_QUERY/load_hawq_tables.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 zcat /retail_demo/customers_dim/customers_dim.tsv.gz | psql -c "COPY retail_demo.customers_dim_hawq FROM STDIN DELIMITER E'\t' NULL E'';"
 zcat /retail_demo/categories_dim/categories_dim.tsv.gz | psql -c "COPY retail_demo.categories_dim_hawq FROM STDIN DELIMITER E'\t' NULL E'';"
 zcat /retail_demo/order_lineitems/order_lineitems.tsv.gz | psql -c "COPY retail_demo.order_lineitems_hawq FROM STDIN DELIMITER E'\t' NULL E'';"
